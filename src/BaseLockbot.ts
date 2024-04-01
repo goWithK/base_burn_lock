147d617eb96c97c9ed1ab6b2fcdb3b7fa95cce55
goWithK <khang.nguyen6461@gmail.com>
2024-04-01 17:31:48 +0700

    Update lock event of UNCX and MC

--- src/BaseLockbot.ts
+++ src/BaseLockbot.ts
@@ -8,14 +8,14 @@ import Web3 from 'web3';
 import { globalConfig, groupConfig, outConfig } from './common/limitsConfig';
 import { BotContext } from './types';
 import { COMMANDS } from './commands';
-import { getBurnTx, getLockInfoMoon } from './commands/burnLPBase';
+import { getBurnTx, getLockInfoMoon, getLockInfoUNCX } from './commands/burnLPBase';
 
 //Env vars
 const BOT_TOKEN = process.env.BURN_ETH_BOT || '';
 const web3 = new Web3(new Web3.providers.HttpProvider(`${process.env.ALCHEMY_ENDPOINT_BASE}`))
 
 //BOT CONFIG
-const bot = new Bot<BotContext>(BOT_TOKEN);
+const bot = new Bot<ParseModeFlavor<BotContext>>(BOT_TOKEN);
 const throttler = apiThrottler({
     global: globalConfig,
     group: groupConfig,
@@ -23,7 +23,7 @@ const throttler = apiThrottler({
 });
 
 bot.api.setMyCommands(COMMANDS);
-// bot.use(hydrateReply);
+bot.use(hydrateReply);
 bot.use(emojiParser());
 bot.api.config.use(throttler);
 //bot.api.config.use(parseMode('')); // Sets default parse_mode for ctx.reply
@@ -40,8 +40,8 @@ bot.use(
 bot.use(
     limit({
         // Allow only 3 messages to be handled every 2 seconds.
-        timeFrame: 2000,
-        limit: 3,
+        timeFrame: 3000,
+        limit: 6,
         // This is called when the limit is exceeded.
         onLimitExceeded: async (ctx) => {
             await ctx.reply('Please refrain from sending too many requests!');
@@ -57,50 +57,63 @@ function delay(time: number) {
     return new Promise(resolve => setTimeout(resolve, time));
 }
 
+const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+});
+
 async function getBurnInfo(trueOrFalse: boolean, chatId: any, ctx: any) {
     if(trueOrFalse) {
         const currentBlock = await web3.eth.getBlockNumber().then(value => { return Number(value) });
         const UrlTransferBurn = `https://api.basescan.org/api?module=logs&action=getLogs&fromBlock=${Number(currentBlock)-3}&toBlock=${currentBlock}&topic0=0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef&topic0_2_opr=and&topic2=0x000000000000000000000000000000000000000000000000000000000000dead&page=1&offset=100&apikey=${process.env.API_BASESCAN_KEY4}`
-        let info = await fetch(UrlTransferBurn).then(
-            response => response.json()
-        ).then(
-            async data => {
-                await delay(1000);
-                if (data['result'] !== 'Error!' && data['result'].length > 0) {
-                    // console.log('Burn: ', data['result'])
-                    let output = [];
-                    for (let i = 0; i < data['result'].length;  i++) {
-                        const txHash = data['result'][i]['transactionHash'];
-                        const burnInfo = await getBurnTx(txHash);
-                        output.push(burnInfo)
+        var responseClone: any;
+        let info = await fetch(UrlTransferBurn)
+        .then(function (response) {
+            responseClone = response.clone(); 
+            return response.json();
+        })
+        .then(async function (data) {
+            await delay(1000);
+            if (data['result'] !== 'Error!' && data['result'].length > 0) {
+                // console.log('Burn: ', data['result'])
+                let output = [];
+                for (let i = 0; i < data['result'].length; i++) {
+                    const txHash = data['result'][i]['transactionHash'];
+                    const burnInfo = await getBurnTx(txHash);
+                    output.push(burnInfo)
 
-                        return output
-                    }
+                    return output
                 }
-            } 
-        );
+            }
+        }, function (rejectionReason) { 
+            console.log('Error parsing JSON from response:', rejectionReason, responseClone);
+            responseClone.text().then(function (bodyText: any) {
+                console.log('Received the following instead of valid JSON:', bodyText); 
+            });
+        });
         //console.log('Info burn:', info)
-        if (info){
+        if (info) {
             for (let i = 0; i < info.length; i++) {
                 let eachInfo = info[i];
                 let holder_msg = 'Holders: ';
-                if (eachInfo){
-                    let title = ctx.emoji`${"fire"} <strong>LP BURNT</strong>  | ${eachInfo[7]} | ${eachInfo[8]} \n \n`
-                    let ca_msg = `<a href="https://basescan.org/address/${eachInfo[0]}">CA:</a> <b>${eachInfo[0]}</b>\n \n`;
-                    let burn_msg = `Liquidity: <b> <strong>${eachInfo[1]}% of Liquidity Burnt</strong>.</b>\n`
+                if (eachInfo) {
+                    let title = ctx.emoji`${"fire"} <b>LP BURNT</b> | ${eachInfo[7]} | ${eachInfo[8]} \n \n`
+                    let ca_msg = `<a href="https://basescan.org/address/${eachInfo[0]}">CA:</a> <code>${eachInfo[0]}</code>\n \n`;
+                    let mc_msg = ctx.emoji`${"bar_chart"} MC: <b>${formatter.format(eachInfo[10])}</b>\n`
+                    let burn_msg = `Liquidity: <b>**${eachInfo[1]}% of Liquidity Burnt**</b>\n`
                     let total_msg = ctx.emoji`${"busts_in_silhouette"} Total holders: ${eachInfo[2]} \n`;
                     let ca_balance_msg = `Clog: ${eachInfo[4]} \n`;
                     let renounced_msg = ctx.emoji`Renounced: ${"cross_mark"} \n`
-                    if (eachInfo[5]) {renounced_msg = ctx.emoji`Renounced: ${"check_mark_button"} \n`}
+                    if (eachInfo[5]) { renounced_msg = ctx.emoji`Renounced: ${"check_mark_button"} \n` }
                     let initLp_msg = ctx.emoji`${"money_with_wings"} Init LP: ${eachInfo[6]} ETH\n`;
                     let holderAddress = Object.keys(eachInfo[3]);
                     let list_msg = [];
                     let holderLimit = 5;
-                    if (holderLimit > holderAddress.length) { holderLimit = holderAddress.length}
-                    for (let i=0; i < holderLimit; i++) {
+                    if (holderLimit > holderAddress.length) { holderLimit = holderAddress.length }
+                    for (let i = 0; i < holderLimit; i++) {
                         list_msg.push(`<a href="https://basescan.org/address/${holderAddress[i]}">${eachInfo[3][holderAddress[i]]}</a>`)
                     }
-                    let tg_msg = title + ca_msg + burn_msg + total_msg + ca_balance_msg + renounced_msg + initLp_msg + holder_msg + list_msg.join(' | ');
+                    let tg_msg = title + ca_msg + burn_msg + mc_msg +total_msg + ca_balance_msg + renounced_msg + initLp_msg + holder_msg + list_msg.join(' | ');
                     await bot.api.sendMessage(
                         chatId,
                         tg_msg,
@@ -119,46 +132,54 @@ async function getLockInfo(trueOrFalse: boolean, chatId: any, ctx: any) {
         const currentBlock = await web3.eth.getBlockNumber().then(value => { return Number(value) });
         const UrlLockTokenMoon = `https://api.basescan.org/api?module=logs&action=getLogs&fromBlock=${Number(currentBlock)-3}&toBlock=${currentBlock}&address=0x77110f67C0EF3c98c43570BADe06046eF6549876&topic0=0x531cba00a411ade37b4ca8175d92c94149f19536bd8e5a83d581aa7f040d192e&page=1&offset=1000&apikey=${process.env.API_BASESCAN_KEY}`
 
-        let info = await fetch(UrlLockTokenMoon).then(
-            response => response.json()
-        ).then(
-            async data => {
-                await delay(1000);
-                let output = [];
-                if (data['result'] !== 'Error!' && data['result'].length > 0) {
-                    // console.log('Lock: ', data['result'])
-                    for (let i = 0; i < data['result'].length;  i++) {
-                        const txHash = data['result'][i]['transactionHash'];
-                        const lockInfo = await getLockInfoMoon(txHash);
-                        output.push(lockInfo)
+        var responseClone1: any;
+        let info1 = await fetch(UrlLockTokenMoon)
+        .then(function (response) {
+            responseClone1 = response.clone(); 
+            return response.json();
+        })
+        .then(async function (data) {
+            await delay(1000);
+            let output = [];
+            if (data['result'] !== 'Error!' && data['result'].length > 0) {
+                // console.log('Lock: ', data['result'])
+                for (let i = 0; i < data['result'].length; i++) {
+                    const txHash = data['result'][i]['transactionHash'];
+                    const lockInfo = await getLockInfoMoon(txHash);
+                    output.push(lockInfo)
 
-                        return output
-                    }
+                    return output
                 }
             }
-        );
+        }, function (rejectionReason) { 
+            console.log('Error parsing JSON from response OnlyMoon:', rejectionReason, responseClone1);
+            responseClone1.text().then(function (bodyText: any) {
+                console.log('Received the following instead of valid JSON:', bodyText); 
+            });
+        });
         //console.log('Info lock:', info)
-        if (info){
+        if (info1) {
             var holder_msg = 'Holders: ';
-            for (let i = 0; i < info.length; i++) {
-                let eachInfo = info[i];
-                if (eachInfo){
-                    let title = ctx.emoji`${"locked"} <strong>LP LOCK</strong> | ${eachInfo[8]} | ${eachInfo[9]} \n\n`
-                    let ca_msg = `<a href="https://basescan.org/address/${eachInfo[0]}">CA:</a> <b>${eachInfo[0]}</b>\n \n`;
-                    let lock_msg = ctx.emoji`<b>${'locked'} Liquidity: <strong>${eachInfo[2]} % of Liquidity Locked for ${eachInfo[1]} days</strong>.</b>\n`;
+            for (let i = 0; i < info1.length; i++) {
+                let eachInfo = info1[i];
+                if (eachInfo) {
+                    let title = ctx.emoji`${"locked"} <b>LP LOCK</b> | ${eachInfo[8]} | ${eachInfo[9]} \n\n`
+                    let ca_msg = `<a href="https://basescan.org/address/${eachInfo[0]}">CA:</a> <code>${eachInfo[0]}</code>\n \n`;
+                    let lock_msg = ctx.emoji`<b>${'locked'} Liquidity: **${eachInfo[2]} % of Liquidity Locked for ${eachInfo[1]} days**</b>\n`;
+                    let mc_msg = ctx.emoji`${"bar_chart"} MC: <b>${formatter.format(eachInfo[11])}</b>\n`
                     let total_msg = ctx.emoji`${"busts_in_silhouette"} Total holders: ${eachInfo[3]} \n`;
                     let ca_balance_msg = `Contract Balance: ${eachInfo[5]}% \n`;
                     let renounced_msg = ctx.emoji`Renounced: ${"cross_mark"} \n`
-                    if (eachInfo[5]) {renounced_msg = ctx.emoji`Renounced: ${"check_mark_button"} \n`}
+                    if (eachInfo[5]) { renounced_msg = ctx.emoji`Renounced: ${"check_mark_button"} \n` }
                     let initLp_msg = ctx.emoji`${"money_with_wings"} Init LP: ${eachInfo[7]} ETH \n`;
                     let holderAddress = Object.keys(eachInfo[4]);
                     let list_msg = [];
                     let holderLimit = 5;
-                    if (holderLimit > holderAddress.length) { holderLimit = holderAddress.length}
-                    for (let i=0; i < holderLimit; i++) {
+                    if (holderLimit > holderAddress.length) { holderLimit = holderAddress.length }
+                    for (let i = 0; i < holderLimit; i++) {
                         list_msg.push(`<a href="https://basescan.org/address/${holderAddress[i]}">${eachInfo[4][holderAddress[i]]}%</a>`)
                     }
-                    let tg_msg = title + ca_msg + lock_msg + total_msg + ca_balance_msg + renounced_msg + initLp_msg + holder_msg + list_msg.join(' | ');
+                    let tg_msg = title + ca_msg + lock_msg + mc_msg + total_msg + ca_balance_msg + renounced_msg + initLp_msg + holder_msg + list_msg.join(' | ');
                     await bot.api.sendMessage(
                         chatId,
                         tg_msg,
@@ -167,7 +188,66 @@ async function getLockInfo(trueOrFalse: boolean, chatId: any, ctx: any) {
                 }
             }
         }
-        await delay(3500);
+
+        const urlLockUNCX = `https://api.basescan.org/api?module=logs&action=getLogs&fromBlock=${startblock}&toBlock=${currentBlock}&address=0x231278eDd38B00B07fBd52120CEf685B9BaEBCC1&topic0=0x3bf9c85fbe37d401523942f10940796acef64062e1a1c45647978e32f4969f5c&page=1&offset=1000&apikey=${process.env.API_BASESCAN_KEY}`
+        var responseClone2: any;
+        let info2 = await fetch(urlLockUNCX)
+        .then(function (response) {
+            responseClone2 = response.clone(); 
+            return response.json();
+        })
+        .then(async function (data) {
+            await delay(1000);
+                let output = [];
+                if (data['result'] !== 'Error!' && data['result'].length > 0) {
+                    // console.log('Lock: ', data['result'])
+                    for (let i = 0; i < data['result'].length; i++) {
+                        const txHash = data['result'][i]['transactionHash'];
+                        const lockInfo = await getLockInfoUNCX(txHash);
+                        output.push(lockInfo)
+
+                        return output
+                    }
+                }
+        }, function (rejectionReason) { 
+            console.log('Error parsing JSON from response UNCX:', rejectionReason, responseClone2);
+            responseClone2.text().then(function (bodyText: any) {
+                console.log('Received the following instead of valid JSON:', bodyText); 
+            });
+        });
+        // console.log('Info lock:', info)
+
+        if (info2) {
+            var holder_msg = 'Holders: ';
+            for (let i = 0; i < info2.length; i++) {
+                let eachInfo = info2[i];
+                if (eachInfo) {
+                    let title = ctx.emoji`${"locked"} <b>LP LOCK</b> | ${eachInfo[8]} | ${eachInfo[9]} \n\n`
+                    let ca_msg = `<a href="https://basescan.org/address/${eachInfo[0]}">CA:</a> <code>${eachInfo[0]}</code>\n \n`;
+                    let lock_msg = ctx.emoji`<b>${'locked'} Liquidity: **${eachInfo[2]} % of Liquidity Locked for ${eachInfo[1]} days**</b>\n`;
+                    let mc_msg = ctx.emoji`${"bar_chart"} MC: <b>${formatter.format(eachInfo[11])}</b>\n`
+                    let total_msg = ctx.emoji`${"busts_in_silhouette"} Total holders: ${eachInfo[3]} \n`;
+                    let ca_balance_msg = `Contract Balance: ${eachInfo[5]}% \n`;
+                    let renounced_msg = ctx.emoji`Renounced: ${"cross_mark"} \n`
+                    if (eachInfo[5]) { renounced_msg = ctx.emoji`Renounced: ${"check_mark_button"} \n` }
+                    let initLp_msg = ctx.emoji`${"money_with_wings"} Init LP: ${eachInfo[7]} ETH \n`;
+                    let holderAddress = Object.keys(eachInfo[4]);
+                    let list_msg = [];
+                    let holderLimit = 5;
+                    if (holderLimit > holderAddress.length) { holderLimit = holderAddress.length }
+                    for (let i = 0; i < holderLimit; i++) {
+                        list_msg.push(`<a href="https://basescan.org/address/${holderAddress[i]}">${eachInfo[4][holderAddress[i]]}%</a>`)
+                    }
+                    let tg_msg = title + ca_msg + lock_msg + mc_msg + total_msg + ca_balance_msg + renounced_msg + initLp_msg + holder_msg + list_msg.join(' | ');
+                    await bot.api.sendMessage(
+                        chatId,
+                        tg_msg,
+                        { parse_mode: "HTML" },
+                    );
+                }
+            }
+        }
+        await delay(2500);
         await getLockInfo(trueOrFalse, chatId, ctx);
     }
 }
