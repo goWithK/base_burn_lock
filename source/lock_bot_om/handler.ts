64c0e6ff617b1edec21a60573a2c91cabc2f511a
goWithK <khang.nguyen6461@gmail.com>
2024-04-14 16:19:38 +0700

    config chat id

--- source/lock_bot_om/handler.ts
+++ source/lock_bot_om/handler.ts
@@ -43,7 +43,7 @@ export class LockBotHandlerOM implements IBotCommand {
 
     public async executeStartCommand(bot: Bot<ParseModeFlavor<BotContext>>, ctx: any): Promise<void> {
         console.log('LOCK OM IS RUNNING');
-        const chatId = -4114916111;
+        const chatId = process.env.CHAT_ID ? process.env.CHAT_ID : -4114916111
 
         while (true) {
             try {
